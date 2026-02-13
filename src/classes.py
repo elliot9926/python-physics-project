5e932afcb72f8c6c77b214670317a2a9a8d44bb8
elliot9926 <elliotelyons@gmail.com>
2026-02-13 11:37:34 +0100

    Implemented integrator and system classes, add test file

--- src/classes.py
+++ src/classes.py
@@ -3,36 +3,96 @@ import vpython as vp    # Seems like a decent library for 3d physics simulations
 
 class Simulation:
         """Highest-level control loop which actually runs the simulation"""
-        def __init__(self, system, dt):
+        def __init__(self, system, dt, total_time, write_positions=False):
              self.system = system
              self.dt = dt # Delta time
              self.time = 0.0 # Starts at 0 when we initialize
+             self.write_positions = write_positions
 
         def step(self, dt): # Steps towards the next moment in time
-             pass
+             self.integrator.step(self.bodies, self.system.gravity_system)
 
-class System(Simulation):
+             if self.write_positions:
+               for body in self.bodies:
+                    body.write_position()
+
+class System:
      """The overall system including the bodies and the forces"""
-     def __init__(self, bodies, integrator):
-          pass
+     def __init__(self, bodies, integrator, gravity_system):
+          self.__bodies = bodies
+          self.integrator = integrator
+          self.gravity_system = gravity_system
+
+     @property
+     def bodies(self):
+          return self.__bodies
      
 
-class  Integrator(Simulation):  # Verlet integrator, I still need to read more about this but it seems like the standard
-     pass
-
-class Body(System):
+class Body:
     """A class to represent a spherical rigid body"""
-    def __init__(self, mass, radius, velocity, position, force):
-        self.mass = mass            # Body's mass in kg
-        self.radius = radius        # Body's radius in meters
-        self.velocity = velocity    # Body's initial velocity as a 3D vector in m/s
-        self.position = position    # Body's position in 3D Cartesian space
-        self.force = force          # The sum of forces currently acting on the body
+     def __init__(self, mass, radius, velocity, position, acceleration = vp.vector(0,0,0)):
+          self.mass = mass                     # Body's mass in kg
+          self.radius = radius                 # Body's radius in meters
+          self.velocity = velocity             # Body's initial velocity as a 3D vector in m/s
+          self.position = position             # Body's position in 3D Cartesian space
+          self.acceleration = acceleration     # Total acceleration of the body
+          self.position_history = []
+    
+
+     def write_position(self):
+         self.position_history.append(self.position)
+     
+
+class GravitySystem:
+     G = 6.6743e-11      # Gravitational constant
+     
+     def __init__(self):
+          pass
+     
+     def compute_accelerations(self, bodies):
+          accelerations = []
+
+          for body_i in enumerate(bodies):
+               total_acceleration = vp.vector(0, 0, 0)      # The sum of accelerations acting on this body 
+
+               for body_j in enumerate(bodies):
+                    if body_i == body_j:                    
+                         continue
+                    
+                    r = body_j.position - body_i.position   # Get the difference vector between the position vectors of the two bodies
+                    distance = vp.mag(r + 1e-10)            # The magnitude of the difference vector is the distance between the bodies
+
+                    total_acceleration += self.G * body_j.mass * r / distance**3     
+               
+               accelerations.append(total_acceleration)
+
+          return accelerations
+          
+     
+
+class  VelocityIntegrator:  # Verlet integrator, I still need to read more about this but it seems like the standard
+     def __init__(self, dt):
+          self.dt = dt
+
+          def step(self, bodies, gravity_system):
+               dt = self.dt
+
+               # 1. Compute current accelerations of all bodies
+               accelerations = gravity_system.compute_accelerations(bodies)
+
+               # 2. Update positions
+               for body, acc in zip(bodies, accelerations):
+                    body.position += body.velocity * dt + 0.5 * acc * dt**2
+
+               # 3. Compute new accelerations
+               new_accelerations = gravity_system.compute_accelerations(bodies)
+
+               # 4. Update velocities
+               for body, acc_old, acc_new in zip(bodies, accelerations, new_accelerations):
+                    body.velocity += 0.5 * (acc_old + acc_new) * dt
 
 
 
-    def update(time):
-        pass
 
 def write_file(system):
      """Updates a file with the current states of all bodies in the system"""
@@ -41,3 +101,4 @@ def write_file(system):
 
 
  
+
