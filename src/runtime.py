31dfc7f38c1dd17c107ebcb34094d04992dad1c4
elliot9926 <elliotelyons@gmail.com>
2026-02-27 10:03:24 +0100

    Added basic realtime visualization

--- src/runtime.py
+++ src/runtime.py
@@ -1,7 +1,32 @@
 import classes
 import vpython as vp
 
-# Our final runtime file
+earth = classes.Body(
+    mass=5.97e24, 
+    radius=6371e3, 
+    velocity=vp.vector(-2.43e4, 1.77e4, 0),     # For now, I'm defining x and y as the plane of earth's orbit
+    position=vp.vector(8.7e10, 1.2e11, 0)
+    )
+    
+sun = classes.Body(
+    mass=1.989e30, 
+    radius=695700e3, 
+    velocity=vp.vector(0, 0, 0), 
+    position=vp.vector(0, 0, 0)
+    )
 
-# An example object to test the class
-earth = classes.Body(5.972e24, 6.371e6, vp.vector(30000, 0, 0), vp.vector(0,0,0))
+sun2 = classes.Body(
+    mass=1.989e29, 
+    radius=695700e3, 
+    velocity=vp.vector(2.43e4, -1.77e4, -3e3),     # For now, I'm defining x and y as the plane of earth's orbit
+    position=vp.vector(-8.7e10, 1.2e11, 10e7)
+    )
+
+bodies = [earth, sun, sun2]
+
+gravity_system = classes.GravitySystem()
+integrator = classes.VelocityIntegrator()
+
+system = classes.System(bodies, integrator, gravity_system)
+
+sim = classes.Simulation(system, 3600*12.0, 3600*24*365.0, True)
--- src/visualization.py
+++ src/visualization.py
@@ -1,1 +1,22 @@
- 
+import vpython as vp
+import runtime
+
+# Initialize a list of vpython objects to represent the bodies
+vis_bodies = []
+
+for body in runtime.bodies:
+    new_sphere = vp.simple_sphere(pos=body.position, radius=body.radius, make_trail=True, retain=50)
+    vis_bodies.append(new_sphere)
+
+dt = runtime.sim.dt
+
+
+while True:
+    vp.rate(30)
+
+    runtime.sim.step()
+
+    for body in runtime.bodies:
+        vis_bodies[runtime.bodies.index(body)].pos = body.position
+
+
